9418dc1caec423b422ef8440f50633c2c6ec36a7
wold21 <wold2180@gmail.com>
2022-03-01 14:49:28 +0900

    - h2 DB connected - 과거형 db 접근 코드 작성(비트때 이렇게 배웠음) - 다형성, 스프링 컨테이너

--- src/main/java/com/project/inflearn/SpringConfig.java
+++ src/main/java/com/project/inflearn/SpringConfig.java
@@ -1,15 +1,24 @@
 package com.project.inflearn;
 
+import com.project.inflearn.repository.JdbcMemberRepository;
 import com.project.inflearn.repository.MemberRepository;
 import com.project.inflearn.repository.MemoryMemberRepository;
 import com.project.inflearn.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 // 수동으로 관리하고 싶을 떄 해당 어노테이션을 씀
 // Component 어노테이션을 가지고 있다.
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +27,11 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+
+        // h2 데이터 베이스로 변경
+        // 스프링이 빈으로 올려 놓은 dataSource를 외부에서 주입 받았다.
+        return new JdbcMemberRepository(dataSource);
     }
 }
