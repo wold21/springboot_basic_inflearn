c3e89f2c6f2881a339e7f7998949cf3784539cc7
wold21 <wold2180@gmail.com>
2022-03-01 15:03:30 +0900

    h2 DB와 붙기

--- src/main/java/com/project/inflearn/repository/JdbcMemberRepository.java
+++ src/main/java/com/project/inflearn/repository/JdbcMemberRepository.java
@@ -10,12 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class JdbcMemberRepository  implements MemberRepository{
+public class JdbcMemberRepository implements MemberRepository {
 
     private final DataSource dataSource;
+
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
@@ -41,6 +43,7 @@ public class JdbcMemberRepository  implements MemberRepository{
             close(conn, pstmt, rs);
         }
     }
+
     @Override
     public Optional<Member> findById(Long id) {
         String sql = "select * from member where id = ?";
@@ -48,25 +51,26 @@ public class JdbcMemberRepository  implements MemberRepository{
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
-try {
-        conn = getConnection();
-        pstmt = conn.prepareStatement(sql);
-        pstmt.setLong(1, id);
-        rs = pstmt.executeQuery();
-        if(rs.next()) {
-            Member member = new Member();
-            member.setId(rs.getLong("id"));
-            member.setName(rs.getString("name"));
-            return Optional.of(member);
-        } else {
-            return Optional.empty();
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
         }
-    } catch (Exception e) {
-        throw new IllegalStateException(e);
-    } finally {
-        close(conn, pstmt, rs);
     }
-}
+
     @Override
     public List<Member> findAll() {
         String sql = "select * from member";
@@ -78,7 +82,7 @@ try {
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
             List<Member> members = new ArrayList<>();
-            while(rs.next()) {
+            while (rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -91,6 +95,7 @@ try {
             close(conn, pstmt, rs);
         }
     }
+
     @Override
     public Optional<Member> findByName(String name) {
         String sql = "select * from member where name = ?";
@@ -102,7 +107,7 @@ try {
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, name);
             rs = pstmt.executeQuery();
-            if(rs.next()) {
+            if (rs.next()) {
                 Member member = new Member();
                 member.setId(rs.getLong("id"));
                 member.setName(rs.getString("name"));
@@ -115,11 +120,12 @@ try {
             close(conn, pstmt, rs);
         }
     }
+
     private Connection getConnection() {
         return DataSourceUtils.getConnection(dataSource);
     }
-    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
-    {
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
         try {
             if (rs != null) {
                 rs.close();
@@ -142,6 +148,7 @@ try {
             e.printStackTrace();
         }
     }
+
     private void close(Connection conn) throws SQLException {
         DataSourceUtils.releaseConnection(conn, dataSource);
     }
